eae34b927bfbb761e3ec8737232f0076c37807a4
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-02-13 17:07:17 -0500

    Update config.go

--- config.go
+++ config.go
@@ -4,77 +4,55 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net"
+	"time"
 )
 
-// Config needs to be documented.
+// Config needs to be documented
 type Config struct {
-	From net.Addr
-
-	Allow []net.IP
-	Deny  []net.IP
-
-	allowAll bool
-	denyAll  bool
-}
-
-// Config needs to be documented.
-type configMiddle struct {
-	Allow []string
-	Deny  []string
+	From     string `json:"from"`
+	To       string `json:"to"`
+	Policies struct {
+		AllowAll bool          `json:"allowAll,omitempty"`
+		DenyAll  bool          `json:"denyAll,omitempty"`
+		Allow    []net.IP      `json:"allow,omitempty"`
+		Deny     []net.IP      `json:"deny,omitempty"`
+		Timeout  time.Duration `json:"timeout,omitempty"`
+	} `json:"policies,omitempty"`
+	Hexdump bool `json:"hexdump,omitempty"`
+	Monitor struct {
+		From bool `json:"from,omitempty"`
+		To   bool `json:"to,omitempty"`
+	} `json:"monitor,omitempty"`
+	Log struct {
+		Stderr bool `json:"stderr,omitempty"`
+		Stdout bool `json:"stdout,omitempty"`
+		File   bool `json:"file,omitempty"`
+	}
 }
 
 // NewDefaultConfig needs to be documented.
-func NewDefaultConfig() *Config {
+func NewDefaultConfig(from, to string) *Config {
 	return &Config{
-		allowAll: true,
+		From: from,
+		To:   to,
 	}
 }
 
 // NewConfigFromFile needs to be documented.
 func NewConfigFromFile(filename string) (*Config, error) {
-	plan, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	configM := &configMiddle{}
-	err = json.Unmarshal(plan, configM)
+	file, err := ioutil.ReadFile(filename)
+
 	if err != nil {
 		return nil, err
 	}
 
 	config := &Config{}
 
-	var (
-		allowAll bool
-		denyAll  bool
-	)
+	err = json.Unmarshal(file, config)
 
-	for _, v := range configM.Allow {
-		if v == "*" { // allow all
-			configM.Allow = nil
-			allowAll = true
-		}
-	}
-
-	for _, v := range configM.Deny {
-		if v == "*" { // allow all
-			configM.Deny = nil
-			denyAll = true
-		}
-	}
-
-	out, err := json.Marshal(configM)
 	if err != nil {
 		return nil, err
 	}
-
-	err = json.Unmarshal(out, config)
-	if err != nil {
-		return nil, err
-	}
-
-	config.allowAll = allowAll
-	config.denyAll = denyAll
 
 	return config, nil
 }
