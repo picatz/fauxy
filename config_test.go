9d531abbe1ca6ccc28485b91c73238a6e6e3331a
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-02-13 17:07:20 -0500

    Update config_test.go

--- config_test.go
+++ config_test.go
@@ -2,8 +2,18 @@ package fauxy
 
 import "testing"
 
+func TestNewDefaultConfig(t *testing.T) {
+	config := NewDefaultConfig("127.0.0.1:80", "127.0.0.1:8080")
+	if config.From != "127.0.0.1:80" {
+		t.Error("got unexpected `From` value in config")
+	}
+	if config.To != "127.0.0.1:8080" {
+		t.Error("got unexpected `To` value in config")
+	}
+}
+
 func TestNewConfigFromFile(t *testing.T) {
-	config, err := NewConfigFromFile("tmp/deny_all.json")
+	config, err := NewConfigFromFile("tmp/example.json")
 	if err != nil {
 		t.Fatal(err)
 	}
